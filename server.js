ea177a64d7ef5cda183104e638b91099e8418412
unknown <rafihobiananyavo@gmail.com>
2024-05-26 09:12:20 +0100

    read zip code data

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import net from 'node:net'
+import fs from 'node:fs'
 
 let server = net.createServer()
 
 server.listen(5000, () => console.log('Server run on port 5000'))
+
+// Read data
+const filename = './data/zipcode.json'
+let data = await fs.promises.readFile(filename, { encoding: 'utf-8'})
+data = JSON.parse(data)
+
+
+
