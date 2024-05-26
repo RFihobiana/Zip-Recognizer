c7f61a41885570bb154246020d86369f7b7fc377
unknown <rafihobiananyavo@gmail.com>
2024-05-26 09:24:37 +0100

    Respond to zipcode requests

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import net from 'node:net'
 import fs from 'node:fs'
+import readline from 'node:readline'
 
 let server = net.createServer()
 
@@ -7,8 +8,39 @@ server.listen(5000, () => console.log('Server run on port 5000'))
 
 // Read data
 const filename = './data/zipcode.json'
-let data = await fs.promises.readFile(filename, { encoding: 'utf-8'})
+let data = await fs.promises.readFile(filename, { encoding: 'utf-8' })
 data = JSON.parse(data)
 
+async function zipcodeResponder(socket) {
+    function output(text, prompt = true) {
+        socket.write(`${text}\n\r`)
+        if (prompt) inputReader.prompt()
+    }
 
+    let inputReader = readline.createInterface({
+        input: socket,
+        output: socket,
+        prompt: '>> ',
+    })
 
+    output('Type zip code number you wish to know about...\n\r"bye" to leave')
+    for await (let inputLine of inputReader) {
+        if (inputLine == 'bye') {
+            output('See you next time!', false)
+            return
+        }
+        else {
+            const city = data[inputLine]
+            output(city ? city : `There is no ${inputLine} zipcode in Madagascar!`)
+        }
+    }
+}
+
+server.on('connection', socket => {
+    zipcodeResponder(socket)
+        .then(socket.end)
+        .catch(err => {
+            console.error(err.message)
+            socket.end()
+        })
+})
