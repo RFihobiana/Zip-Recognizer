bba3a821f89bcbf63c2dddfd63d21f6784c092c6
unknown <rafihobiananyavo@gmail.com>
2024-05-26 08:58:19 +0100

    create connection socket for client

---
+++ client.js
@@ -0,0 +1,8 @@
+import net from 'node:net'
+import process from 'node:process'
+
+let socket = net.createConnection(5000, process.argv[2] ?? 'localhost')
+socket.pipe(process.stdout)
+process.stdin.pipe(socket)
+
+socket.on('close', process.exit)
